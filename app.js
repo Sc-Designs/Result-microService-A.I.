ef16f582f20f474af11e153311e5a97d055ec112
sc-designs <152136634+Sc-Designs@users.noreply.github.com>
2025-08-12 10:38:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ connectWithRetry();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
-app.use("/", resultRouter);
+app.use("/",(req, res)=>{
+  res.send("Health Check");
+});
+app.use("/api", resultRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
